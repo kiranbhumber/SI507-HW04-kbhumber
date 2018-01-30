b0f989089ffde4b457c6c4778bb45606b8b139cc
agrgn42 <angela.schopke@gmail.com>
2018-01-29 20:32:45 -0500

    trying a push to master

--- magic_eight.py
+++ magic_eight.py
@@ -1,7 +1,17 @@
+import random
 
 def question():
-    quest= input("What is your question?:")
-    return quest
+    quest= input("What is your question? ")
+    possible_answers = random.choice(["It is certain",
+                        "It is decidedly so",
+                        "Without a doubt",
+                        "Yes definitely",
+                        "You may rely on it",
+                        "As I see it, yes",
+                        "Most likely",
+                        "Outlook good",
+                        "Yes",
+                        "Signs point to yes"])
+    return possible_answers
 
-the_question = question()
-print(the_question)
+print(question())
