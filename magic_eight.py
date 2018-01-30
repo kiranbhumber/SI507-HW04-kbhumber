c9f6d8cd759fdd7cb90f761e64ef7de3b96e55cf
Penghao-He <hopaul@umich.edu>
2018-01-29 21:11:22 -0500

    second edit

--- magic_eight.py
+++ magic_eight.py
@@ -1,7 +1,17 @@
+import random
 
 def question():
-    quest= input("What is your question?:")
-    return quest
+    quest= input("What is your question? ")
+    possible_answers = random.choice(["Reply hazy try again",
+                        "Ask again later",
+                        "Better not tell you now",
+                        "Cannot predict now",
+                        "Concentrate and ask again",
+                        "Don't count on it",
+                        "My reply is no",
+                        "My sources say no",
+                        "Outlook not so good",
+                        "Very doubtful"])
+    return possible_answers
 
-the_question = question()
-print(the_question)
+print(question())
